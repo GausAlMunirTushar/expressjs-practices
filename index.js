4b6b85c886ad24232a354949609246eabf3a3e61
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-09 21:45:21 +0600

    request and response practices

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ app.get('/re', function(req,res){
 app.get('/download',function(req, res){
     res.download('./index.html');
 });
+app.get('/header', function(req, res){
+    res.append("name", "Gaus Al Munir ");
+    res.append("age", "21");
+    res.append("cars", "BMW");
+    res.status(200).send("Header Set");
+    res.end();
+});
+app.get('/cookie', function(req, res){
+    res.cookie("name", "Gaus Al Munir");
+    res.cookie("age", "21");
+    res.cookie("City", "Dhaka");
+    res.end("Cookie Set");
+});
+app.get('/clear', function(req, res){
+    res.clearCookie("name" , "Gaus Al Munir");
+    res.clearCookie("age", "21");
+    res.end("Cookie Clear");
+});
 app.listen(port, function() {
     console.log(`app listening on port http://localhost:${port}`)
 });
