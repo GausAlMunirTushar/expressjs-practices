e9f8878b06ce5ced96b714cffc8a8d9102876571
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-12 11:51:32 +0600

    post and get request

--- index.js
+++ index.js
@@ -3,47 +3,27 @@ const app = express();
 const port = 8080;
 
 app.get('/', function (req, res){
-    res.send('Hello, Home Page');
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.end(firstName + " " + lastName);
 });
-app.get('/about', function (req, res){
-    res.send('About page');
-});
-// Response Status Code
-app.get('/three',function(req, res){
-    res.status(404).send('Sorry, cant find that!');
-}); 
-let person = {
-	"name":"John",
-	"age":30,
-	"cars":[ "Ford", "BMW", "Fiat" ]
-}
-app.get('/person', function (req, res){
-    res.json(person);
-});
-app.get('/re', function(req,res){
-    res.redirect('/about');
+app.get('/get', function(req, res){
+    let firstName = req.header('firstName');
+    let lastName = req.header('lastName');
+    let Connection = req.header('Connection')
+    res.end(firstName + " " + lastName + " connection :" + Connection);
 })
-app.get('/download',function(req, res){
-    res.download('./index.html');
+app.post('/post', (req, res) => {
+    let userName = req.header('userName');
+    let password = req.header('password');
+    res.end("UserName:" + userName + " Password:" + password);
 });
-app.get('/header', function(req, res){
-    res.append("name", "Gaus Al Munir ");
-    res.append("age", "21");
-    res.append("cars", "BMW");
-    res.status(200).send("Header Set");
-    res.end();
-});
-app.get('/cookie', function(req, res){
-    res.cookie("name", "Gaus Al Munir");
-    res.cookie("age", "21");
-    res.cookie("City", "Dhaka");
-    res.end("Cookie Set");
-});
-app.get('/clear', function(req, res){
-    res.clearCookie("name" , "Gaus Al Munir");
-    res.clearCookie("age", "21");
-    res.end("Cookie Clear");
-});
+app.post('/query', function(req, res){
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.end(firstName + " " + lastName);
+})
+
 app.listen(port, function() {
     console.log(`app listening on port http://localhost:${port}`)
 });
--- request.js
+++ request.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.end(firstName + " " + lastName);
+});
+app.get('/query',(req,res)=>{
+   
 });
 
 
-app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`));
+app.listen(port, () => console.log(`Exam app listening on port http://localhost:${port}`));
